55c3bfa29f4a8403a4b2706aa581fff7d68befcc
Fractanomics <fractanomics@gmail.com>
2026-08-11 15:58:11 +0200

    chore: bump version to v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.fractanomics.crosstraining"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.1.0"
+        versionCode = 12
+        versionName = "2.2.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
